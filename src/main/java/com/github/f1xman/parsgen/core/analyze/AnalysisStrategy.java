4c3d46b5fcea018175675054f2467bf32e63c14c
Timur Yarosh <tyarosh@voiptime.net>
2021-08-10 22:24:31 +0300

    remove inline LoadedPage and bump version to 0.2.0

--- src/main/java/com/github/f1xman/parsgen/core/analyze/AnalysisStrategy.java
+++ src/main/java/com/github/f1xman/parsgen/core/analyze/AnalysisStrategy.java
@@ -1,12 +1,11 @@
 package com.github.f1xman.parsgen.core.analyze;
 
-import com.github.f1xman.parsgen.core.analyze.model.LoadedPage;
 import com.github.f1xman.parsgen.core.analyze.model.PageFeatures;
 
 import java.net.URL;
 
 public interface AnalysisStrategy {
-    PageFeatures analyze(LoadedPage page);
+    PageFeatures analyze(URL url, String content);
 
     boolean supports(URL url);
 }
--- src/main/java/com/github/f1xman/parsgen/core/analyze/model/LoadedPage.java
+++
@@ -1,8 +0,0 @@
-package com.github.f1xman.parsgen.core.analyze.model;
-
-import com.github.f1xman.parsgen.core.analyze.AnalysisStrategyNotFoundException;
-import com.github.f1xman.parsgen.core.analyze.LoadedPageAnalyzer;
-
-public interface LoadedPage {
-    PageFeatures analyze(LoadedPageAnalyzer analyzer) throws AnalysisStrategyNotFoundException;
-}
